69d746c273f227b91001eeb6ec75fda7c1ee41a5
BlaKCronic <poncegonzalez849@gmail.com>
2025-03-18 17:03:30 -0600

    main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
